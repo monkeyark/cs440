cfc698b07fe8ec0ab4ea404c63ee8980461dce64
monkeyark <monkeyark@gmail.com>
2023-02-02 17:20:09 -0600

    example code

---
+++ Homework/hw1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define BUFFERSIZE 4
+
+void read_int(FILE *in, char *lexeme, unsigned max_length)
+{
+	char c;
+	for (;;)
+	{
+		c = fgetc(in);
+		if (c == EOF) return;
+		//load file to buffer
+		
+		printf("%c",c);
+	}
+}
+
+
+int main(int argc, char** argv)
+{
+	FILE *infile = fopen("infile", "r");
+	char *lexeme;
+	read_int(infile, lexeme, BUFFERSIZE);
+	printf("%s", lexeme);
+	return 0;
+}
