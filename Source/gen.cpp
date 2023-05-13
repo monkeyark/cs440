c5f8dc53d948c8b8e00cbac6ce3dc4398961cde7
Zhi Wang <danryw@iastate.edu>
2023-05-13 11:55:43 -0500

    add code generation

---
+++ Source/gen.cpp
@@ -0,0 +1,13 @@
+#include "gen.hpp"
+#include "type.hpp"
+
+
+// call type_check_file(string filename, bool is_output) in "type.hpp" to start part1-4
+
+
+void gen_file(std::string filename, bool is_output)
+{
+	type_check_file(filename, false);//comment out this line to only test part4-5
+	//TODO
+
+}
---
+++ Source/gen.hpp
@@ -0,0 +1,8 @@
+#ifndef AST_HPP
+#define AST_HPP
+
+#include<string>
+
+void gen_file(std::string filename, bool is_output);
+
+#endif
